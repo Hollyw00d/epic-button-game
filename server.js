8cb10eee1156bbc68b93d8e86ef96b907f4fb120
Hollyw00d <matt@mattjennings.net>
2015-07-15 19:56:39 -0700

    assgnmnt done

--- server.js
+++ server.js
@@ -17,7 +17,59 @@ app.get("/", function(req, res) {
     res.render("index");
 });
 
+app.get("/reset", function(req, res) {
+    res.redirect("/");
+});
+
 // Express is listening on port 6789
 var server = app.listen(6789, function() {
     console.log("Node.js is running!");
+});
+
+var io = require("socket.io").listen(server);
+
+// Setting the counter to 0
+var counter = 0;
+
+io.sockets.on("connection", function(socket) {
+    console.log("Sockets are running!");
+
+    socket.on("client_count", function(action) {
+        counter++;
+        console.log(counter);
+
+        socket.emit("server_counter", {response: counter});
+    });
+
+
+
+
+
+
+    socket.on("client_reset_count", function(action) {
+        counter = 0;
+
+        socket.emit("server_reset_count", {zero: counter});
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
